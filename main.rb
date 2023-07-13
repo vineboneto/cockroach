a300b151e8e7c90284d75d203100d7b485be750e
vineboneto <vineboneto@gmail.com>
2023-07-12 21:35:55 -0300

    refactor: remove unused comments

--- main.rb
+++ main.rb
@@ -1,19 +1,19 @@
 require 'pg'
 require 'logger'
 
-logger = Logger.new('logfile.log') # Especifique o nome do arquivo de log desejado
+logger = Logger.new('logfile.log')
 
 begin
   conn = PG.connect(host:'haproxy', dbname: 'defaultdb', user: 'root', password: 'haproxy', port: 26257)
 
   while true do
     res = conn.exec('select now()')
-    logger.info(res.getvalue(0, 0)) # Utilize o logger para registrar a saída
+    logger.info(res.getvalue(0, 0))
     sleep 1
   end
 
 rescue PG::Error => e
-  logger.error(e.message) # Utilize o logger para registrar erros
+  logger.error(e.message)
 ensure
   conn.close if conn
 end
